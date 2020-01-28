a002d2ef02f4f056d12ea4ae8328d5edeaaff9a0
Kamil Czerwinski <kamilcze31@gmail.com>
2020-01-28 19:16:19 +0100

    repair v2

--- src/test/java/pl/krakow/uek/pp5/kijo/home/creditcard/model/CreditCardApiTest.java
+++ src/test/java/pl/krakow/uek/pp5/kijo/home/creditcard/model/CreditCardApiTest.java
@@ -7,53 +7,43 @@ import pl.krakow.uek.pp5.kijo.home.creditcard.model.exceptions.NotEnoughMoneyExc
 
 import java.math.BigDecimal;
 
-public class CreditCardTest {
+mport java.math.BigDecimal;
 
-    public static final int LIMIT = 2000;
+public class CreditCardApiTest {
 
-    @Test
-    public void itAllowAssignCreditToCard() {
-        //Arrange / Given
-        CreditCard card = new CreditCard("1234-5678");
-        //Act / When
-        card.assignLimit(BigDecimal.valueOf(LIMIT));
-        //Assert / Then
-        Assert.assertTrue(card.getLimit().equals(BigDecimal.valueOf(LIMIT)));
-    }
+    public static final String CREDIT_CARD_NUMBER = "1234-5678";
+    public static final BigDecimal CC_LIMIT = BigDecimal.valueOf(1000);
+    private InMemoryCreditCardStorage storage;
+    private CreditCardFacade api;
 
     @Test
-    public void creditBelowGeneralLimitIsNotPossible() {
-        //Arrange
-        CreditCard card = new CreditCard("1234-5678");
-        //Act
+    public void iAmAbleToWithdrawFromCard() {
+        thereIsCCPermanentStorage();
+        thereIsCreditCard(CREDIT_CARD_NUMBER);
+        thereIsCCApi();
 
-        //Assert
-        try {
-            card.assignLimit(BigDecimal.valueOf(50));
-            Assert.fail("exception should be thrown");
-        } catch (CreditBelowLimitException e) {
-            Assert.assertTrue(true);
-        }
+        api.withdraw(new WithdrawCommand(CREDIT_CARD_NUMBER, BigDecimal.valueOf(200)));
+
+        ccWithNumberBalanceEquals(CREDIT_CARD_NUMBER, BigDecimal.valueOf(800));
     }
 
-    @Test
-    public void withdrawFromCard(){
-        CreditCard card1 = new CreditCard("1234-5678");
-        CreditCard card2 = new CreditCard("1234-5679");
-        card1.assignLimit(BigDecimal.valueOf(1000));
-        card2.assignLimit(BigDecimal.valueOf(1000));
-        card1.withdraw(BigDecimal.valueOf(500));
-        card2.withdraw(BigDecimal.valueOf(100));
-        Assert.assertEquals(BigDecimal.valueOf(500),card1.getCurrentBalance());
-        Assert.assertEquals(BigDecimal.valueOf(900),card2.getCurrentBalance());
+    private void ccWithNumberBalanceEquals(String creditCardNumber, BigDecimal moneyLeft) {
+        CreditCard cc = storage.load(creditCardNumber);
+        Assert.assertEquals(cc.getCurrentBalance(), moneyLeft);
     }
 
-    @Test(expected = NotEnoughMoneyException.class)
-    public void denyWithdrawBelowBalance() {
-        CreditCard card = new CreditCard("1234-5678");
-        card.assignLimit(BigDecimal.valueOf(1000));
+    private void thereIsCCPermanentStorage() {
+        this.storage = new InMemoryCreditCardStorage();
+    }
 
-        card.withdraw(BigDecimal.valueOf(600));
-        card.withdraw(BigDecimal.valueOf(600));
+    private void thereIsCreditCard(String creditCardNumber) {
+        CreditCard cc = new CreditCard(creditCardNumber);
+        cc.assignLimit(CC_LIMIT);
+
+        this.storage.add(cc);
+    }
+
+    private void thereIsCCApi() {
+        this.api = new CreditCardFacade(this.storage);
     }
 }
